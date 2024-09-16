8517aa60bd65debbb486a3864dd0c40dd080add1
Gabriel Zério <21043296+gabrielzerio@users.noreply.github.com>
2024-09-16 20:02:47 -0300

    alterando o diretorio do js

--- js/cliente/cliente.js
+++ js/cliente/cliente.js
@@ -2,7 +2,7 @@ export class Cliente {
     constructor(nome, email, cpf) {
       this.nome = nome;
       this.email = email;
-      this.cpf = cpf
+      this.cpf = cpf;
       this.saldo;
     }
    
